84f32c933d69c4c91d6a71019e87cb519c4d6d82
lpoli99 <lucianopoli2805@gmail.com>
2022-10-16 18:26:17 -0300

    Agregué operadores avanzados y más repuestos.

--- js/index.js
+++ js/index.js
@@ -24,6 +24,7 @@ items.addEventListener('click', e =>{
     btnAction(e);
 });
 
+//FETCH REPUESTOS
 const fetchSpareparts = async () => {
     const res = await fetch('../js/repuestos.json');
     const data = await res.json();
@@ -31,6 +32,7 @@ const fetchSpareparts = async () => {
     
 }
 
+//MOSTRAR ITEMS MODIFICANDO DOM
 const showSpareparts = data => {
     data.forEach(product =>{
         templateCard.querySelector('h5').textContent = product.titulo;
@@ -38,25 +40,25 @@ const showSpareparts = data => {
         templateCard.querySelector('p').textContent = product.descripcion;
         templateCard.querySelector('img').setAttribute("src", product.imagen);
         templateCard.querySelector('.btn-dark').dataset.id = product.id;
-
         const clone = templateCard.cloneNode(true);
         fragment.appendChild(clone);
     });
     sparePartscontainer.appendChild(fragment); 
 }
 
+//Agregar items al carrito
 const addCart = e => {
-    if(e.target.classList.contains('btn-dark')){
-        setCart(e.target.parentElement);
-    }
+    e.target.classList.contains('btn-dark') && setCart(e.target.parentElement);
     e.stopPropagation();
     Toastify({
         text: "Item añadido al carrito!",
         duration: 1800,
         gravity: "top",
-        position: "left"
+        position: "left",
+        style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+        }
     }).showToast();
-
 }
 
 const setCart = object => {
@@ -66,16 +68,15 @@ const setCart = object => {
         price: object.querySelector('h6').textContent,
         quantity: 1
     }
-
     if(cart.hasOwnProperty(product.id)) {
         product.quantity = cart[product.id].quantity + 1;
     }
-
     cart[product.id] = {...product};
     showCart();
 
 }
 
+//MOSTRAR ITEMS EN EL CARRITO
 const showCart = () => {
     items.innerHTML = '';
     Object.values(cart).forEach(product => {
@@ -85,17 +86,15 @@ const showCart = () => {
         templateCart.querySelector('.btn-info').dataset.id = product.id;
         templateCart.querySelector('.btn-danger').dataset.id = product.id;
         templateCart.querySelector('span').textContent = product.quantity * product.price;
-        
         const clone = templateCart.cloneNode(true);
         fragment.appendChild(clone)
     })
     items.appendChild(fragment);
-
     showFooter();
-
     localStorage.setItem('parts', JSON.stringify(cart));
 }
 
+//Evento vaciar carrito 
 const showFooter = () => {
     footer.innerHTML = '';
     if(Object.keys(cart).length === 0) {
@@ -104,16 +103,13 @@ const showFooter = () => {
         `
         return;
     }
-
     const nQuantity = Object.values(cart).reduce((acc, {quantity}) => acc + quantity, 0);
     const nPrice = Object.values(cart).reduce((acc, {quantity, price}) => acc + quantity * price, 0);
-
     templateFooter.querySelectorAll('td')[0].textContent = nQuantity;
     templateFooter.querySelector('span').textContent = nPrice;
     const clone = templateFooter.cloneNode(true);
     fragment.appendChild(clone);
     footer.appendChild(fragment);
-
     const btnEmptyCart = document.getElementById('vaciar-carrito');
     btnEmptyCart.addEventListener('click', () => {
         cart = {};
@@ -122,11 +118,15 @@ const showFooter = () => {
             text: "Carrito vaciado!",
             duration: 1800,
             gravity: "top",
-            position: "left"
+            position: "left",
+            style: {
+                background: "linear-gradient(to right, #F1959B, #DC1C13)",
+            }
         }).showToast();
     });
 }
 
+//CAMBIAR CANTIDAD DE ITEMS EN EL CARRITO
 const btnAction = e => {
     // Aumentar
     if(e.target.classList.contains('btn-info')) {
@@ -138,22 +138,26 @@ const btnAction = e => {
             text: "Item añadido al carrito!",
             duration: 1800,
             gravity: "top",
-            position: "left"
+            position: "left",
+            style: {
+                background: "linear-gradient(to right, #00b09b, #96c93d)",
+            }
         }).showToast();
     }
     //Disminuir
     if(e.target.classList.contains('btn-danger')) {
         const product = cart[e.target.dataset.id];
         product.quantity--;
-        if(product.quantity === 0) {
-            delete cart[e.target.dataset.id];
-        }
+        product.quantity === 0 && delete cart[e.target.dataset.id];
         showCart();
         Toastify({
             text: "Item eliminado del carrito!",
             duration: 1800,
             gravity: "top",
-            position: "left"
+            position: "left",
+            style: {
+                background: "linear-gradient(to right, #F1959B, #DC1C13)",
+            }
         }).showToast();
     }
     e.stopPropagation();
@@ -163,11 +167,7 @@ const btnAction = e => {
 btnSwitch.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     btnSwitch.classList.toggle('active');
-    if (document.body.classList.contains('dark')) {
-        localStorage.setItem('modo-oscuro', 'true');
-    }else {
-        localStorage.setItem('modo-oscuro', 'false');
-    }
+    document.body.classList.contains('dark') ? localStorage.setItem('modo-oscuro', 'true') : localStorage.setItem('modo-oscuro', 'false');
 });
 
 if(localStorage.getItem('modo-oscuro') === 'true'){
